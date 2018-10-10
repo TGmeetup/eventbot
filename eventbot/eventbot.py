b6592edeb1959913a959b9c741b2af2971dc3b4c
sufuf3 <sufuf3@gmail.com>
2018-10-10 14:04:53 +0800

    Update add event logic

--- eventbot/eventbot.py
+++ eventbot/eventbot.py
@@ -45,19 +45,27 @@ def main():
         for i in localevent:
             add_event = True
             for j in issuelist:
+                # If local event is on GitHub issue, remove the adding event list from memory
                 if j["name"] == i["name"] and j["datetime"] == i["datetime"]:
                     issue_list.remove(j)
                     add_event = False
+            # If local event is not on GitHub issue, search again
+            # If found the local event is on GitHub, add_event is False
+            if issuehandle.get_issue(i["name"], "Event") == True:
+                add_event = False
+            # Finally, if add_event is True, let's add this local event on GitHub issue
             if add_event is True:
                 detail_event = localhandle.get_event_detail(i)
                 issuehandle.add_issue(detail_event, i["group_ref"])
 
+    # Close the GitHub issue which need to close from memory(30)
     if len(issue_list) > 0:
         for i in issue_list:
             if datetime.strptime(i["datetime"].split("T")[0],
                                  '%Y-%m-%d') < datetime.now():
                 issuehandle.close_issue(i["number"])
-
+    # Search GitHub issue by title < Today and close these GitHub issue
+    # Todo..
 
 if __name__ == '__main__':
     main()
